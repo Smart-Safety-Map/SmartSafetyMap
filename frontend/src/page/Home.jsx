349941d33a5fb1d29c6e070cc5c0fdc6dfa4fd2d
leejm <leejm010813@gmail.com>
2025-07-17 21:22:42 +0900

    위치정보 반영 v1

--- frontend/src/page/Home.jsx
+++ frontend/src/page/Home.jsx
@@ -1,20 +1,8 @@
-import { useEffect, useRef } from "react";
+import React from 'react';
+import MapHook from 'src/hooks/MapHook';
 
 const Home = () => {
-    const mapRef = useRef(null);
-
-    useEffect(() => {
-        if (window.naver && window.naver.maps && mapRef.current) {
-            // 맵 객체가 이미 있으면 중복 생성 방지
-            if (!mapRef.current.__naver_map) {
-                mapRef.current.__naver_map = new window.naver.maps.Map(mapRef.current, {
-                    center: new window.naver.maps.LatLng(37.3595704, 127.105399),
-                    zoom: 10,
-                });
-            }
-        }
-    }, []);
-
+    const {mapRef, myLocation} = MapHook();
     // 주: style, id 대신 ref={mapRef} 사용
     return (
         <>
