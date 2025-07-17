a9556d751504bdb85dd1c0ddedf06233c8822785
dytpq <suhokym@gmail.com>
2025-07-17 21:31:49 +0900

    시큐리티 전체 요청 허용

---
+++ backend/src/main/java/SmartSafetyMap/backend/domain/HomeTestController.java
@@ -0,0 +1,14 @@
+package SmartSafetyMap.backend.domain;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HomeTestController {
+
+    @GetMapping("/")
+    public String home() {
+        return "Hello World";
+    }
+
+}
